7801d6851d07ded3807ee099994ac8354ec9922a
alex <48170421+nandbolt@users.noreply.github.com>
2023-10-21 21:21:46 -0500

    text repeater

--- ai/ai_agent.js
+++ ai/ai_agent.js
@@ -1,4 +1,39 @@
-class AI
+class AIAgent
 {
-	
+
+	constructor()
+    {
+        console.log('(new AI agent created)');
+    }
+
+    // Input message
+    inputMessage(message)
+    {
+        console.log('input message: ' + message);
+        this.outputMessage(message);
+    }
+
+    // Output message
+    outputMessage(message)
+    {
+        console.log('output message: ' + message);
+        return message;
+    }
+}
+
+
+// Test loop
+const prompt = require('prompt-sync')();
+var ai = new AIAgent();
+while (true)
+{
+    // Input
+    var input = prompt('Type message (type "exit" to quit): ');
+    if (input.includes('exit'))
+    {
+        break;
+    }
+
+    // Output
+    var output = ai.inputMessage(input);
 }
